73843edde7c2fe47e8edcdad903c6149599dcf68
Nandra Saputra <nandra.saputras@gmail.com>
2020-02-10 20:08:41 +0700

    V 0.9.0
    + ChannelFragment with ViewPager

--- app/src/main/java/com/nandra/myschool/ui/channel/ChannelFragment.kt
+++ app/src/main/java/com/nandra/myschool/ui/channel/ChannelFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
 import com.nandra.myschool.R
+import com.nandra.myschool.adapter.ChannelViewPagerAdapter
 import kotlinx.android.synthetic.main.channel_fragment.*
 
 class ChannelFragment : Fragment() {
+
+    private lateinit var channelViewPagerAdapter: ChannelViewPagerAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.channel_fragment, container, false)
@@ -20,6 +24,7 @@ class ChannelFragment : Fragment() {
         }.apply {
             supportActionBar?.setDisplayShowTitleEnabled(false)
         }
+        setupViewPager()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -30,5 +35,23 @@ class ChannelFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
+    }
+
+    private fun setupViewPager() {
+        channelViewPagerAdapter = ChannelViewPagerAdapter(
+            childFragmentManager,
+            fragment_channel_tab_layout.tabCount
+        )
+        fragment_channel_viewpager.adapter = channelViewPagerAdapter
+
+        fragment_channel_tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                fragment_channel_viewpager.currentItem = tab!!.position
+            }
+        })
+
+        fragment_channel_viewpager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(fragment_channel_tab_layout))
     }
 }
